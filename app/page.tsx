58adf341ca79eeb68d78a8362869776df78f6289
twesigetrejan <trejantwesige19@gmail.com>
2025-01-11 14:30:36 +0300

    done

--- app/page.tsx
+++ app/page.tsx
@@ -24,27 +24,29 @@ export default async function Home() {
       <div className="grid grid-cols-3 place-items-center">
         {articles &&
           articles.map((article) => (
-            <div
-              key={article.title}
-              className="flex flex-col gap-4 max-w-[320px] bg-gray-50 px-2 py-4 rounded-lg"
-            >
-              <h2 className=" text-xl">{article.title}</h2>
-              <div className="max-h-[260px]">
-                <Image
-                  src={urlFor(article.titleImage).url()}
-                  width={300}
-                  height={260}
-                  className="object-cover rounded-md "
-                  alt={"Blog post image"}
-                />
-              </div>
-              <p className="line-clamp-3">{article.smallDescription}</p>
-              <div className="flex justify-between items-center">
-                <p className="">By: {article.author}</p>
-                <div className="">
-                  <button className="bg-blue-500 py-1 px-1 text-white rounded-md hover:scale-105">
-                    Read more
-                  </button>
+            <div className="pb-10" key={article.title}>
+              <div
+                key={article.title}
+                className="flex flex-col gap-4 max-w-[320px] bg-gray-50 px-2 py-4 rounded-lg"
+              >
+                <h2 className=" text-xl">{article.title}</h2>
+                <div className="max-h-[260px]">
+                  <Image
+                    src={urlFor(article.titleImage).url()}
+                    width={300}
+                    height={260}
+                    className="object-cover rounded-md "
+                    alt={"Blog post image"}
+                  />
+                </div>
+                <p className="line-clamp-3">{article.smallDescription}</p>
+                <div className="flex justify-between items-center">
+                  <p className="">By: {article.author}</p>
+                  <div className="">
+                    <button className="bg-blue-500 py-1 px-1 text-white rounded-md hover:scale-105">
+                      Read more
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
